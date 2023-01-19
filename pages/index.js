6b3cb05f052837efec13a2fed3ca36f450ac9793
Meelaud Totonchi <mtotonchi@se14l0120-15.ad.bcit.ca>
2023-01-19 15:51:02 -0800

    second commit

--- pages/index.js
+++ pages/index.js
@@ -47,28 +47,10 @@ export default function Home() {
           </div>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+        
 
         <div className={styles.grid}>
-          <div>Business Degrees:</div>
+          <div class="titles text-background">Business Degrees:</div>
           {
             information && information.map((info, index) => {
               if (info.department === "Business") {
@@ -79,7 +61,7 @@ export default function Home() {
             })
           }
 
-          <div>Computing Degrees:</div>
+          <div class="titles text-background">Computing Degrees:</div>
           {
             information && information.map((info, index) => {
               if (info.department === "Computing") {
